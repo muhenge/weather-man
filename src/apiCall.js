ee5f0280f2ead3aedeac340f0d7d93e8e31b6d7d
herve <mugunga5@gmail.com>
2021-02-28 11:17:52 +0200

    Add temp

--- src/apiCall.js
+++ src/apiCall.js
@@ -1,8 +1,15 @@
+
+
 const apiCall = async(input) => {
-    const dataAPI = `https://api.openweathermap.org/data/2.5/weather?q=${input}&appid=7892818979a2e87a17bcda46fe0dbc53`;
-    const response = await fetch(dataAPI);
-    const data = await response.json();
-    console.log(data);
+    document.getElementById('submit-btn').addEventListener('click', async(e)=>{
+        e.preventDefault();
+        const dataAPI = `https://api.openweathermap.org/data/2.5/weather?q=${input}&appid=7892818979a2e87a17bcda46fe0dbc53`;
+        const response = await fetch(dataAPI);
+        const data = await response.json();
+        console.log(data.main.temp);
+       
+        document.getElementById('val').textContent = Math.round(data.main.temp_max-273);
+    })
 }
 
 export default apiCall;
--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ document.getElementById('city-input').addEventListener('input',(e) => {
     try {
         apiCall(e.target.value);
     } catch (error) {
-        console.log(error);
+        console.error(error);
     }
 });
 
