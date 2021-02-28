c91d788e1f5193ccf2f8222e037a88c66c20b53a
herve <mugunga5@gmail.com>
2021-02-28 19:47:10 +0200

    Add weather icon container

--- src/apiCall.js
+++ src/apiCall.js
@@ -2,13 +2,19 @@
 
 const apiCall = async(input) => {
     document.getElementById('submit-btn').addEventListener('click', async(e)=>{
+        const iconContainer = document.getElementById('icon');
         e.preventDefault();
         const dataAPI = `https://api.openweathermap.org/data/2.5/weather?q=${input}&appid=7892818979a2e87a17bcda46fe0dbc53`;
         const response = await fetch(dataAPI);
         const data = await response.json();
-        console.log(data.main.temp);
-        document.getElementById('val').textContent = Math.round(data.main.temp_max-273);
-    })
+        console.log(data.weather[0].description)
+        if(e.target) {
+            document.getElementById('temp').innerHTML = `<span class="display-1 text-muted">${Math.round(data.main.temp_max-273)}&#176C</span>`;
+            if (data.weather[0].description === 'scattered clouds') {
+                iconContainer.innerHTML = '<img src="./images/cloud.png" class="img-fluid img-thumbnail"/>'
+            }
+        }
+    });
 }
 
 export default apiCall;
