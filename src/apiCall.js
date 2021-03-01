87f7edfc7c15a6fe998edde91c6a9ef544d6ab9f
herve <mugunga5@gmail.com>
2021-03-01 15:22:28 +0200

    Correct eslint error

--- src/apiCall.js
+++ src/apiCall.js
@@ -2,16 +2,14 @@ const apiCall = async (input) => {
   document.getElementById('submit-btn').addEventListener(
     'click',
     async (e) => {
-      const iconContainer = document.getElementById('icon');
       const dataAPI = `https://api.openweathermap.org/data/2.5/weather?q=${input}&appid=7892818979a2e87a17bcda46fe0dbc53`;
       const response = await fetch(dataAPI);
       const data = await response.json();
       const iconcode = data.weather[0].icon;
-      const iconurl = "http://openweathermap.org/img/w/" + iconcode + ".png";
+      const iconurl = `http://openweathermap.org/img/w/${iconcode}.png`;
       const icon = document.createElement('img');
       icon.setAttribute('src', `${iconurl}`);
       icon.classList.add('icon');
-      console.log(data);
       if (e.target) {
         document.getElementById('info').innerHTML = `
             <span class="display-1 text-white">${Math.round(data.main.temp_max - 273)}&#176C</span><br>
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import apiCall from './apiCall';
 
+// eslint-disable-next-line consistent-return
 document.getElementById('city-input').addEventListener('input', (e) => {
   try {
     apiCall(e.target.value);
