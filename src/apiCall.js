f2251ff4291c27b29e2d3f93c80a7416efae834e
herve <mugunga5@gmail.com>
2021-03-06 00:17:50 +0200

    Fix lint error

--- src/apiCall.js
+++ src/apiCall.js
@@ -18,20 +18,18 @@ const getWeatherData = async (city) => {
     document.getElementById('f').addEventListener('click', (e) => {
       e.preventDefault();
       const celsius = Math.round(data.main.temp_max - 273);
-      if(e.target) {
+      if (e.target) {
+        // eslint-disable-next-line no-mixed-operators
         const fahr = celsius * 9 / 5 + 32;
         document.getElementById('temp').innerHTML = `<strong> ${fahr} &#176F</strong>`;
       }
-    })
+    });
     document.getElementById('temp').innerHTML = `<strong> ${Math.round(data.main.temp_max - 273)} &#176C</strong>`;
     document.getElementById('forecast').innerText = `${data.weather[0].description}`;
   } catch (error) {
-    document.getElementById('not-found').innerHTML = '<span class="display-3 text-danger">City not found</span>'
+    document.getElementById('not-found').innerHTML = '<span class="display-3 text-danger">City not found</span>';
   }
 };
-
-
-
 
 const apiCall = () => {
   document.getElementById('submit-btn').addEventListener('click', (e) => {
--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
 import apiCall from './apiCall';
 
-// eslint-disable-next-line consistent-return
 apiCall();
